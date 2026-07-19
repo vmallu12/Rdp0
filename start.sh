faaacef16091490f061706e9859dcfa2496722cf
vmallu12 <130160386+vmallu12@users.noreply.github.com>
2026-07-20 00:06:08 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,41 +1,41 @@
 #!/bin/bash
 
 export DISPLAY=:1
+export MOZ_ENABLE_WAYLAND=0
+export MOZ_DISABLE_GPU_SANDBOX=1
+export MOZ_WEBRENDER=0
+export LIBGL_ALWAYS_SOFTWARE=1
+export MOZ_USE_XINPUT2=1
 
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix
 
-# Start virtual display
-Xvfb :1 -screen 0 1920x1080x24 &
+Xvfb :1 -screen 0 1280x720x24 &
 sleep 2
 
-# Start D-Bus
 mkdir -p /run/dbus
 dbus-daemon --system --fork
 
-# Start XFCE desktop
 startxfce4 &
-
 sleep 5
 
-# Start VNC
 x11vnc \
-  -display :1 \
-  -forever \
-  -shared \
-  -nopw \
-  -listen 0.0.0.0 \
-  -xkb \
-  -rfbport 5900 &
+-display :1 \
+-forever \
+-shared \
+-nopw \
+-listen 0.0.0.0 \
+-rfbport 5900 \
+-noxdamage \
+-repeat \
+-xkb \
+-wait 10 &
 
-# Start SSH
 /usr/sbin/sshd
 
-# Start noVNC
 websockify \
-  --web=/usr/share/novnc \
-  8080 \
-  localhost:5900 &
+--web=/usr/share/novnc \
+8080 \
+localhost:5900 &
 
-# Keep services running
-exec /usr/bin/supervisord -n -c /etc/supervisor/supervisord.conf
+exec /usr/bin/supervisord -n
