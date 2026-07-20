9deb5f602adece031e5861e32561e97221739ee2
vmallu12 <130160386+vmallu12@users.noreply.github.com>
2026-07-20 08:21:34 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 export DISPLAY=:1
+export HOME=/root
+
+# Firefox optimizations
 export MOZ_ENABLE_WAYLAND=0
 export MOZ_DISABLE_GPU_SANDBOX=1
 export MOZ_WEBRENDER=0
@@ -10,32 +13,45 @@ export MOZ_USE_XINPUT2=1
 mkdir -p /tmp/.X11-unix
 chmod 1777 /tmp/.X11-unix
 
-Xvfb :1 -screen 0 720x1600x24 &
+# Virtual display (good for mobile)
+Xvfb :1 -screen 0 1280x720x24 -ac +extension GLX +render -noreset &
 sleep 2
 
+# D-Bus
 mkdir -p /run/dbus
 dbus-daemon --system --fork
 
+# XFCE
+export DISPLAY=:1
 startxfce4 &
 sleep 5
 
+# VNC
 x11vnc \
 -display :1 \
 -forever \
 -shared \
 -nopw \
--listen 0.0.0.0 \
 -rfbport 5900 \
+-listen 0.0.0.0 \
 -noxdamage \
 -repeat \
 -xkb \
--wait 10 &
+-wait 10 \
+-bg
 
+# SSH
 /usr/sbin/sshd
 
+# noVNC
 websockify \
 --web=/usr/share/novnc \
 8080 \
 localhost:5900 &
 
-exec /usr/bin/supervisord -n
+echo "Desktop Started"
+echo "Open:"
+echo "https://YOUR-DOMAIN/vnc.html?autoconnect=1&resize=scale&quality=9&compression=9"
+
+# Keep container alive
+exec tail -f /dev/null
